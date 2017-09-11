e958563423c603ec9629baa08dbd85aedb736527
Connor Kuehl <cipkuehl@gmail.com>
2017-09-11 09:14:48 -0700

    Initial outline of Window class.

--- src/window.h
+++ src/window.h
@@ -1,10 +1,23 @@
 #ifndef WINDOW_H_
 #define WINDOW_H_
 
+#include <string>
+
+#include "SDL.h"
+
 class Window
 {
     public:
+        Window(size_t width, size_t height, 
+                const std::string& title = "");
+       ~Window();
     private:
+       std::string title_;
+       size_t width_;
+       size_t height_;
+       SDL_Window* window_;
+
+       void cleanup();
 };
 
 #endif // WINDOW_H_
