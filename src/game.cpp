9458ba1578e6d92545b37b6389ebfeaf31c4a323
Connor Kuehl <cipkuehl@gmail.com>
2017-09-11 09:36:49 -0700

    Basic game loop for testing purposes.

--- src/game.cpp
+++ src/game.cpp
@@ -1,2 +1,43 @@
 #include "game.h"
 
+Game::Game(const std::string& name, size_t width, size_t height)
+: window_{width, height, name}
+, isRunning_ { true }
+{
+    renderer_.attachToWindow(window_.get());
+}
+
+void Game::run()
+{
+    auto last = SDL_GetTicks();
+    while (isRunning_) {
+        auto now = SDL_GetTicks();
+        auto elapsed = now - last;
+        last = now;
+        interact();
+        update(elapsed);
+        draw();
+    }
+}
+
+void Game::interact()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            isRunning_ = false;
+        }
+    }
+}
+
+void Game::update(Uint32 elapsed)
+{
+
+}
+
+void Game::draw()
+{
+    renderer_.clear();
+    renderer_.show();
+}
+
--- src/game.h
+++ src/game.h
@@ -1,11 +1,30 @@
 #ifndef GAME_H_
 #define GAME_H_
 
-class Game
+#include "sdl_application.h"
+#include "window.h"
+#include "renderer.h"
+
+namespace
+{
+    const int DEFAULT_WIDTH = 800;
+    const int DEFAULT_HEIGHT = 600;
+}
+
+class Game : public SdlApplication
 {
     public:
+        Game(const std::string& name, size_t width = DEFAULT_WIDTH, 
+                size_t height = DEFAULT_HEIGHT);
+        void run();
     private:
-        // 
+        Window window_;
+        Renderer renderer_;
+        bool isRunning_;
+
+        void interact();
+        void update(Uint32 elapsed);
+        void draw();
 };
 
 #endif // GAME_H_
