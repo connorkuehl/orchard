d918c595888d05b95e193b2afdfc9d394a3f2821
Connor Kuehl <cipkuehl@gmail.com>
2017-09-11 08:58:35 -0700

    Create Renderer class.

---
+++ src/renderer.h
@@ -0,0 +1,20 @@
+#ifndef RENDERER_H_
+#define RENDERER_H_
+
+#include "SDL.h"
+
+class Renderer
+{
+    public:
+        Renderer();
+       ~Renderer();
+
+       void attachToWindow(SDL_Window& window);
+       void clear();
+       void show();
+    private:
+       SDL_Renderer* renderer_;
+};
+
+#endif // RENDERER_H_
+
