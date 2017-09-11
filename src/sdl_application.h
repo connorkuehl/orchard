5a012dee07cc274e4530dfcc4ecc357e2ba62b54
Connor Kuehl <cipkuehl@gmail.com>
2017-09-10 22:44:36 -0700

    Create SdlApplication class.

---
+++ src/sdl_application.h
@@ -0,0 +1,12 @@
+#ifndef SDL_APPLICATION_H_
+#define SDL_APPLICATION_H_
+
+class SdlApplication
+{
+    public:
+        SdlApplication();
+       ~SdlApplication();
+};
+
+#endif //SDL_APPLICATION_H_
+
