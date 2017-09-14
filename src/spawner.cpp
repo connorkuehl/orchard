4786939ab667133cda1b7d02318362f4f8d41443
Connor Kuehl <cipkuehl@gmail.com>
2017-09-13 20:45:23 -0700

    Create Spawner class.

---
+++ src/spawner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Connor Kuehl on 9/13/17.
+//
+
+#include "spawner.h"
---
+++ src/spawner.h
@@ -0,0 +1,12 @@
+#ifndef ORCHARD_SPAWNER_H
+#define ORCHARD_SPAWNER_H
+
+
+class Spawner
+{
+    public:
+    private:
+};
+
+
+#endif //ORCHARD_SPAWNER_H
