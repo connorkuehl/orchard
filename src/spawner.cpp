d66d50099b128a7fc06015a557fad266fe6118c6
Connor Kuehl <cipkuehl@gmail.com>
2017-09-14 08:45:10 -0700

    Complete initial implementation of spawner... it spawns apples.

--- src/spawner.cpp
+++ src/spawner.cpp
@@ -1,23 +1,53 @@
 #include <random>
+#include <algorithm>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <iostream>
 #include "game_constants.h"
 #include "spawner.h"
 
-Spawner::Spawner()
-: spawnedThisSecond_{0}
+Spawner::Spawner(float initialSpawnDelay)
+: prototype_{{0, 0}}
+, spawnDelay_{initialSpawnDelay}
 {
 }
 
-void Spawner::update()
+void Spawner::attachPrototype(Apple &prototype)
 {
-    if (timer_.getElapsedTime().asSeconds() >= 1) {
+    prototype_ = prototype;
+}
+
+void Spawner::update(float elapsed)
+{
+    if (timer_.getElapsedTime().asSeconds() >= spawnDelay_) {
+        spawn();
         timer_.restart();
-        // spawn
+    }
+
+    for (auto& apple : apples_) {
+        apple.update(elapsed);
+    }
+
+    std::remove_if(apples_.begin(), apples_.end(),
+    [](const auto &apple) {
+        return apple.isDead();
+    });
+}
+
+void Spawner::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    for (const auto& apple : apples_) {
+        target.draw(apple);
     }
 }
 
 void Spawner::spawn()
 {
-    if (spawnedThisSecond_ < APPLES_PER_SECOND) {
-        ++spawnedThisSecond_;
-    }
+    apples_.emplace_back(prototype_.clone(randomPosition()));
+}
+
+sf::Vector2f Spawner::randomPosition() const
+{
+    static std::default_random_engine engine{};
+    static std::uniform_real_distribution<float> range{0, game::SCREEN_WIDTH};
+    return {range(engine), 0.f};
 }
--- src/spawner.h
+++ src/spawner.h
@@ -6,20 +6,24 @@
 
 namespace
 {
-    const auto APPLES_PER_SECOND = 2.f;
+    const auto DEFAULT_SPAWN_DELAY = 1.75f;
 }
 
-class Spawner
+class Spawner : public sf::Drawable
 {
     public:
-        Spawner();
-        void update();
+        Spawner(float initialSpawnDelay = DEFAULT_SPAWN_DELAY);
+        void attachPrototype(Apple &prototype);
+        void update(float elapsed);
+        void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
     private:
+        Apple prototype_;
         sf::Clock timer_;
         std::vector<Apple> apples_;
-        int spawnedThisSecond_;
+        float spawnDelay_;
 
         void spawn();
+        sf::Vector2f randomPosition() const;
 };
 
 #endif //ORCHARD_SPAWNER_H
