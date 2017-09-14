ffec757b944112d47a4eaa6a0560bbe774e0cc50
Connor Kuehl <cipkuehl@gmail.com>
2017-09-13 21:28:15 -0700

    Continue outlining spawner.

--- src/spawner.cpp
+++ src/spawner.cpp
@@ -1,5 +1,23 @@
-//
-// Created by Connor Kuehl on 9/13/17.
-//
-
+#include <random>
+#include "game_constants.h"
 #include "spawner.h"
+
+Spawner::Spawner()
+: spawnedThisSecond_{0}
+{
+}
+
+void Spawner::update()
+{
+    if (timer_.getElapsedTime().asSeconds() >= 1) {
+        timer_.restart();
+        // spawn
+    }
+}
+
+void Spawner::spawn()
+{
+    if (spawnedThisSecond_ < APPLES_PER_SECOND) {
+        ++spawnedThisSecond_;
+    }
+}
--- src/spawner.h
+++ src/spawner.h
@@ -1,12 +1,25 @@
 #ifndef ORCHARD_SPAWNER_H
 #define ORCHARD_SPAWNER_H
 
+#include <vector>
+#include "apple.h"
+
+namespace
+{
+    const auto APPLES_PER_SECOND = 2.f;
+}
 
 class Spawner
 {
     public:
+        Spawner();
+        void update();
     private:
-};
+        sf::Clock timer_;
+        std::vector<Apple> apples_;
+        int spawnedThisSecond_;
 
+        void spawn();
+};
 
 #endif //ORCHARD_SPAWNER_H
