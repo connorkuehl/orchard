87d48478fcf954ca48020b864988ddbd5b252b67
Connor Kuehl <cipkuehl@gmail.com>
2017-09-23 20:56:32 -0700

    Create Main Menu Scene class.

---
+++ src/main_menu_scene.cpp
@@ -0,0 +1,34 @@
+#include <SFML/Window/Event.hpp>
+#include "engine/scene_manager.h"
+#include "main_menu_scene.h"
+#include "play_scene.h"
+
+MainMenuScene::MainMenuScene(sf::RenderWindow & window, SceneManager & sceneManager)
+: Scene(window, sceneManager)
+{
+    mainMenu_.setTexture(resources_.loadTexture("res/main_menu.png"));
+}
+
+void MainMenuScene::interact()
+{
+    sf::Event event;
+    while (window_.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            shouldExit(true);
+        }
+        else if (event.type == sf::Event::KeyPressed) {
+            sceneManager_.push(new PlayScene(window_, sceneManager_), true);
+        }
+    }
+}
+
+void MainMenuScene::update(float elapsed)
+{
+}
+
+void MainMenuScene::draw()
+{
+    window_.clear();
+    window_.draw(mainMenu_);
+    window_.display();
+}
---
+++ src/main_menu_scene.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_MENU_SCENE_H_
+#define MAIN_MENU_SCENE_H_
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include "engine/scene.h"
+
+class MainMenuScene : public Scene
+{
+    public:
+        explicit MainMenuScene(sf::RenderWindow & window, SceneManager & sceneManager);
+        ~MainMenuScene() override = default;
+
+        void interact() override;
+        void update(float elapsed) override;
+        void draw() override;
+    private:
+        sf::Sprite mainMenu_;
+};
+
+#endif // MAIN_MENU_SCENE_H_
